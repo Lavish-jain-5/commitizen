514103a4e16a8346c4cd1e051e67282de96aeb10
Lavish <lavish@email.com>
2023-07-05 00:31:11 +0530

    fix: 33 commm

--- test2.py
+++ test2.py
@@ -1,5 +1,19 @@
-print('jhello world')
-print('dfkf')
-print('fkfk')
-print('sjdkkkkkkkkkkkk')
-print('a')
+
+import subprocess
+import os
+import re
+
+def get_bumped_version():
+    result = subprocess.run(["cz", "bump", "--check-consistency", "--changelog"], capture_output=True, text=True)
+    output = result.stdout
+    match = re.search(r"bump: version (\S+) → (\S+)", output)
+    if match:
+        bumped_version = match.group(2)
+        subprocess.call(["git", "tag", bumped_version])
+    else:
+        return None
+
+def main():
+    # Execute your post-commit actions here
+    print("Running post-commit actions...")
+    get_bumped_version()
