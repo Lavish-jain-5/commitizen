0b2d1b2359dcfb7189af70bba08496bfe2b04b36
Lavish <lavish@email.com>
2023-07-05 00:11:03 +0530

    feat: 23 commmit

--- test2.py
+++ test2.py
@@ -1,27 +1,29 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import subprocess
-import os
-import re
+# import subprocess
+# import os
+# import re
 
-def get_bumped_version():
-    result = subprocess.run(["cz", "bump", "--dry-run", "--check-consistency", "--changelog"], capture_output=True, text=True)
-    print(result)
-    output = result.stdout
-    print(output)
-    match = re.search(r"bump: version (\S+) → (\S+)", output)
-    print(match)
-    if match:
-        return match.group(2)
-    else:
-        return None
+# def get_bumped_version():
+#     result = subprocess.run(["cz", "bump", "--dry-run", "--check-consistency", "--changelog"], capture_output=True, text=True)
+#     print(result)
+#     output = result.stdout
+#     print(output)
+#     match = re.search(r"bump: version (\S+) → (\S+)", output)
+#     print(match)
+#     if match:
+#         return match.group(2)
+#     else:
+#         return None
 
-def main():
-    # Execute your post-commit actions here
-    print("Running post-commit actions...")
-    bumped_version = get_bumped_version()
-    if bumped_version:
-        subprocess.call(["git", "tag", bumped_version])  # Create the Git tag
+# def main():
+#     # Execute your post-commit actions here
+#     print("Running post-commit actions...")
+#     bumped_version = get_bumped_version()
+#     if bumped_version:
+#         subprocess.call(["git", "tag", bumped_version])  # Create the Git tag
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+
+
+#     main()
